33cfdf375e948c8834a9be25863108f86304db42
Jake Potterf <Jacob.Potterf@gmail.com>
2019-07-02 13:54:17 +0300

    updated form and test users

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,11 +1,17 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import EventDashboard from "../../features/event/EventDashboard/EventDashboard";
+import NavBar from "../../features/nav/navBar/NavBar";
+import { Container } from "semantic-ui-react";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>What's happening near me?</h1>
-      </div>
+      <Fragment>
+        <NavBar />
+        <Container className='main'>
+          <EventDashboard />
+        </Container>
+      </Fragment>
     );
   }
 }
